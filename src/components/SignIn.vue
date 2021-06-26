31c1dd17f971bd87255c7b752bda3ebfe61aacd9
Antonin <antonin.jacob@viacesi.fr>
2021-06-27 00:40:32 +0200

    axios login request + Store login token + error handling

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -7,12 +7,14 @@
         <br>
         <el-button v-on:click="validate">Submit</el-button>
         <br>
-        <p>{{ errorMessage }}</p>
+        <p class="danger">{{ errorMessage }}</p>
     </div>
 </template>
 
 <script lang="ts">
     import { defineComponent } from 'vue'
+    import AuthService from '@/services/AuthService.ts'
+    import { ElMessage } from 'element-plus';
 
     const Login = defineComponent({
         name: 'Login',
@@ -32,7 +34,25 @@
                              "";
                 
                 this.errorMessage = errMessage;
+
+                if (!errMessage) this.signIn();
             },
+            async signIn() {
+                try {
+                    const credentials = {
+                        Email: this.email,
+                        Password: this.password,
+                    };
+                    const response = await AuthService.signIn(credentials);
+                    const token = response.token;
+                    const user = response.user;
+
+                    this.$store.dispatch('login', { token, user });
+
+                } catch (error) {
+                    ElMessage.error(error.response.data.message);
+                }
+            }
         }
     });
     export default Login;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-/* Vue native */
+/* Core */
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Axios from 'axios';
 /* Other libraries */
 // Element UI Library (https://element-plus.org/#/en-US)
 import 'element-plus/lib/theme-chalk/index.css';
 import ElementPlus from 'element-plus';
+
+// set auth header
+Axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 
 createApp(App)
     .use(router)
